ec69e6fce796480b3df34052be4a1ea715579394
John Cranney <john.cranney@gmail.com>
2021-08-04 18:11:06 +1000

    working

--- rewrite.py
+++ rewrite.py
@@ -1,7 +1,8 @@
 import fitz
+from datetime import date,timedelta
 
 class Page:
-    def __init__(self, title="",titlex=20,titley=250,titlesize=50,basepdfname="",toclevel=0):
+    def __init__(self, title="",titlex=20,titley=250,titlesize=50,basepdfname="",toclevel=0,links=[]):
         self.title = title 
         self.linksets=[]
         self.pageno=0
@@ -14,7 +15,7 @@ class Page:
         self.toclevel=toclevel
         
     
-    def addLinkSet(self,*linksets):
+    def addLinks(self,*linksets):
         for linkset in linksets:
             self.linksets.append(linkset)
         return self
@@ -32,7 +33,7 @@ class Page:
 
 
 
-class LinkSet:
+class Links:
     #An abstract class to represent a set of outbound links.
     #
     #Primary responsibilities are:
@@ -52,9 +53,8 @@ class LinkSet:
         self.pages.append(page);
         
 
-
-class HorizontalLinkSet(LinkSet):
-    # Renders a set of links left to right or right to left,
+class LinearLinks(Links):
+    # Renders a set of links left to right, or top to bottom,
     # as a set of boxes, containing centered label text.
     #
     # Expects one of left / right to be passed in constructor
@@ -76,14 +76,17 @@ class HorizontalLinkSet(LinkSet):
     # The size of each box can be modified by the width and height args-
     # Note that is the label text is too large it will just not rendered
     #
+    # Use flowdirection="right" to choose left to right, 
+    # and flowdirection="down" to choose top to bottom
     # The fontsize is also controllable.
-    def __init__(self,width=80,height=80,left="",top="",right="",bottom="",fontsize=30):
+    def __init__(self,width=80,height=80,left="",top="",right="",bottom="",flowdirection="right",fontsize=30):
         super().__init__()
 
         self.left=left
         self.top=top
         self.right=right
         self.bottom=bottom
+        self.flowdirection=flowdirection
 
         if (left=="" and right==""):
             raise Exception("You must set either left or right starting points")
@@ -103,28 +106,49 @@ class HorizontalLinkSet(LinkSet):
     # This method will automatically display a box as inverse color 
     # if the link points back to itself.
     def render(self,page):
-    
-        
-        if (self.left!=""):
-            l=self.left 
-        else:
-            if (self.right <= 0): #right is expressed as negative offset from right edge
-                self.right=page.fitzpage.rect.x1+self.right
-            l=self.right-len(self.pages)*self.width
+        if self.flowdirection=="right":
 
-        r=l+self.width
+            if (self.left!=""):
+               l=self.left 
+            else:
+                if (self.right <= 0): #right is expressed as negative offset from right edge
+                    #take the right edge, substract the passed offset, then start
+                    l=page.fitzpage.rect.x1+self.right -len(self.pages)*self.width
 
-        if (self.top!=""):
-            t=self.top  
-        else:
-            if (self.bottom <= 0): #bottom is expressed as negative offset from bottom edge
-                self.bottom=page.fitzpage.rect.y1+self.bottom
-            t=self.bottom-self.height
-        b=t+self.height
+            r=l+self.width
+
+            if (self.top!=""):
+                t=self.top  
+            else:
+                if (self.bottom <= 0): #bottom is expressed as negative offset from bottom edge
+                    #self.bottom=page.fitzpage.rect.y1+self.bottom
+                    t=page.fitzpage.rect.y1+self.bottom -self.height
+            b=t+self.height
+        else: #flow down from top
+            if (self.left!=""):
+               l=self.left 
+            else:
+                if (self.right <= 0): #right is expressed as negative offset from right edge
+                    #take the right edge, substract the passed offset, then start
+                    l=page.fitzpage.rect.x1+self.right -self.width
+
+            r=l+self.width
+
+            if (self.top!=""):
+                t=self.top  
+            else:
+                if (self.bottom <= 0): #bottom is expressed as negative offset from bottom edge
+                    #self.bottom=page.fitzpage.rect.y1+self.bottom
+                    t=page.fitzpage.rect.y1+self.bottom -self.height*len(self.pages)
+            b=t+self.height
+
+
+
+
 
         boxcol=fitz.utils.getColor("black")
 
-        for target in self.pages:
+        for target in self.pages :
 
             if (target.pageno==page.pageno):
                 textcol=fitz.utils.getColor("white")
@@ -135,14 +159,20 @@ class HorizontalLinkSet(LinkSet):
             r1 = fitz.Rect(l, t, r, b)
             textrect = fitz.Rect(l, t+(self.height/2)-(self.fontsize/2*1.33), r, b)
             page.fitzpage.draw_rect(r1,color=boxcol, fill=backcol, overlay=True)
-            r=r+self.width
-            l=l+self.width
+
+            if (self.flowdirection=="right"): 
+                r=r+self.width
+                l=l+self.width
+            else:
+                t=t+self.height
+                b=b+self.height
+                
 
 
             linkdict = {
                 "kind": 1,
                 "from": r1,
-                "page": target.pageno  #need right page number here from links
+                "page": target.pageno  
             }
             page.fitzpage.insert_link(linkdict)
 
@@ -193,65 +223,87 @@ class Doc:
         for page in self.pages:
             page.render(self.fitzdoc);
         self.fitzdoc.set_toc(self.toc, collapse=1)
-        self.fitzdoc.save("/Users/john/Dropbox/Supernote/INBOX/testout.pdf")
+        self.fitzdoc.save("/Users/john/Dropbox/Supernote/INBOX/Week 31 - 1st Aug 2020.pdf")
 
 def buildDoc():
+
+    sunday=date.fromisoformat('2021-08-01')
+    thisweek=f"Week {sunday.strftime('%U, %Y')}"
+    
+    lastSunday=sunday+timedelta(days=-7)
+    lastweek=f"Week {lastSunday.strftime('%U, %Y')}"
+
+
 
     doc=Doc("pagetemplate.pdf")
 
     # links between top level weekly pages
-    weeklyLinkSet=HorizontalLinkSet(left=10,top=110)
+    weeklyLinks=LinearLinks(left=10,top=110)
 
     # links down to daily goal pages from weekly pages
-    dailyGoalsLinkSet=HorizontalLinkSet(right=-10,top=110)
+    dailyGoalsLinks=LinearLinks(right=-10,top=110)
 
     # Build top level weekly pages, with templates, and give them their outbound links
     # Note the daily goal pages do not exist yet, but that's ok!
-    weeklyRetro=Page(title="Retro",basepdfname="weeklyRetroTemplate.pdf",toclevel=1).addLinkSet(weeklyLinkSet,dailyGoalsLinkSet)
-    weeklyDump1=Page(title="Weekly Dump 1",toclevel=1).addLinkSet(weeklyLinkSet,dailyGoalsLinkSet)
-    weeklyDump2=Page(title="Weekly Dump 2",toclevel=1).addLinkSet(weeklyLinkSet,dailyGoalsLinkSet)
-    weeklyGoals=Page(title="Weekly Goals",basepdfname="weeklyGoalsTemplate.pdf",toclevel=1).addLinkSet(weeklyLinkSet,dailyGoalsLinkSet)
+    weeklyRetro=Page(title=f"Retro for {lastweek}",basepdfname="weeklyRetroTemplate.pdf",toclevel=1).addLinks(weeklyLinks,dailyGoalsLinks)
+    weeklyPlanner=Page(title=f"Planner for {thisweek}",basepdfname="weeklyPlannerTemplate.pdf",toclevel=1).addLinks(weeklyLinks,dailyGoalsLinks)
+    weeklyDump1=Page(title=f"Dump 1 for {thisweek}",toclevel=1).addLinks(weeklyLinks,dailyGoalsLinks)
+    weeklyDump2=Page(title=f"Dump 2 for {thisweek}",toclevel=1).addLinks(weeklyLinks,dailyGoalsLinks)
+    weeklyGoals=Page(title=f"Goals for {thisweek}",basepdfname="weeklyGoalsTemplate.pdf",toclevel=1).addLinks(weeklyLinks,dailyGoalsLinks)
 
     # Add pages into the doc
-    doc.addPages(weeklyRetro,weeklyDump1,weeklyDump2,weeklyGoals)
+    doc.addPages(weeklyRetro,weeklyPlanner,weeklyDump1,weeklyDump2,weeklyGoals)
 
     # Link top level pages to each other
-    weeklyLinkSet.addPage(weeklyRetro,"R");
-    weeklyLinkSet.addPage(weeklyDump1,"D1");
-    weeklyLinkSet.addPage(weeklyDump2,"D2");
-    weeklyLinkSet.addPage(weeklyGoals,"G");
+    weeklyLinks.addPage(weeklyRetro,"R");
+    weeklyLinks.addPage(weeklyPlanner,"P");
+    weeklyLinks.addPage(weeklyDump1,"D1");
+    weeklyLinks.addPage(weeklyDump2,"D2");
+    weeklyLinks.addPage(weeklyGoals,"G");
 
     # Build one goals page and 9 notes pages for each day of the week
-    days=['Monday','Tuesday','Wednesday','Thursday','Friday']
-    for day in days:
+    days=[]
+    for x in range(1,6):
+        tempdate=sunday+timedelta(days=x)
+        days.append(tempdate)
+
+
+    for daydate in days:
+        day=daydate.strftime('%a %-d %b %Y')
         # Each set of daily notes has links to each other
-        dailyNotesLinkSet=HorizontalLinkSet(bottom=-5,right=-5)
+        dailyNotesLinks=LinearLinks(bottom=-500,right=-5,flowdirection="down")
 
         # First page is a goals page. 
         # Each of the daily pages links back up to the weekly pages, to the other days in the week, and to each other on this day
-        dailyGoals=Page(title=f"{day}: Daily Goals",basepdfname="dailyGoalsTemplate.pdf",toclevel=1).addLinkSet(weeklyLinkSet,dailyGoalsLinkSet,dailyNotesLinkSet)
+        dailyGoals=Page(title=f"{day}: Daily Goals",basepdfname="dailyGoalsTemplate.pdf",toclevel=1).addLinks(weeklyLinks,dailyGoalsLinks,dailyNotesLinks)
         
         # This gets linked "down to" from the weekly pages above
-        dailyGoalsLinkSet.addPage(dailyGoals,f"{day[0]}")
+        dailyGoalsLinks.addPage(dailyGoals,f"{day[0]}")
 
         # It is also linked by each other page on this day
-        dailyNotesLinkSet.addPage(dailyGoals,"G")
+        dailyNotesLinks.addPage(dailyGoals,"G")
         
         # Add page to doc
         doc.addPages(dailyGoals)
 
+        #Add index page
+        dailyNote=Page(title=f"{day}: Notes Index").addLinks(weeklyLinks,dailyGoalsLinks,dailyNotesLinks)
+        doc.addPages(dailyNote)
+        dailyNotesLinks.addPage(dailyNote,f"I")
 
-        for pageno in range(1,10):
+
+
+        for pageno in range(2,10):
             # These are the individual note pages for a given day 
-            dailyNote=Page(title=f"{day} Notes {pageno}").addLinkSet(weeklyLinkSet,dailyGoalsLinkSet,dailyNotesLinkSet)
+            dailyNote=Page(title=f"{day}: Notes {pageno}").addLinks(weeklyLinks,dailyGoalsLinks,dailyNotesLinks)
             doc.addPages(dailyNote)
-            dailyNotesLinkSet.addPage(dailyNote,f"{pageno}")
+            dailyNotesLinks.addPage(dailyNote,f"{pageno}")
 
     doc.render() 
 
 
 
 
-   
+ 
 
 buildDoc()
